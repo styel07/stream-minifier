e945826aac380f218aaa214d3d2f625ef7d4ca3e
styel07 <eleazarcorpuz@gmail.com>
2015-10-19 10:04:34 -1000

    added a read fs.file and write file function

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
 var fs = require('fs');
 var util = require('util');
+
+function minify() {
+  fs.readFile('test.html', function(error, contentsOfFile) {
+
+
+  });
+
+  // transform stream
+
+  fs.writeFile('./min/', 'test.html', function(err) {
+    if (err) {
+      throw err;
+    } else {
+      console.log('success');
+    }
+  });
+}
