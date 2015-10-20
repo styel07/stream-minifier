f2d4e6919ad889afc2f4ecb8758e0561b5bd470e
styel07 <eleazarcorpuz@gmail.com>
2015-10-20 10:22:13 -1000

    Included a prototype on the ReadStream Class that checks if the user is inputting data

--- server.js
+++ server.js
@@ -10,16 +10,32 @@ var ReadStream = function() {
   this.curIndex = 0;
 };
 
+// This is a prototype method of ReadStream which reads user input
+ReadStream.prototype._read = function() {
+  // checks if the input is null
+  if (this.curIndex === this.data.length) {
+    // when null is returned it means its the end of a input stream
+    return this.push(null);
+  }
+
+  // if the input is not null keep reading by chunk
+  var data = this.data[this.curIndex++];
+
+  // stringify the data coming in
+  console.log('read' + JSON.stringify(data));
+  this.push(data);
+};
+
 // transform stream
 
 // write stream
-fs.writeFile('./min/', 'test.html', function(err) {
-  if (err) {
-    throw err;
-  } else {
-    console.log('success');
-  }
-});
+// fs.writeFile('./min/', 'test.html', function(err) {
+//   if (err) {
+//     throw err;
+//   } else {
+//     console.log('success');
+//   }
+// });
 
 // pipe them together
 //read.pipe(transform).pipe(write);
